8e5257ab819406d69d959fc136029e9181c6ebe8
Nursultan <nursultanyelemessov@gmail.com>
2023-09-11 19:30:45 +0600

    Initial Commit

---
+++ AppEatEyezers/AppEatEyezersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppEatEyezersApp.swift
+//  AppEatEyezers
+//
+//  Created by Nursultan Yelemessov on 11/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct AppEatEyezersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
